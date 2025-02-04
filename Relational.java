48f9ee1371357df95d8932c81ae9553864e1f9c3
surajkrnayak <sn1009505@gmail.com>
2025-02-04 07:14:10 -0800

    demo

---
+++ Relational.java
@@ -0,0 +1,22 @@
+public class Relational {
+    public static void main(String[] args){
+        int a =5;
+        int b=6;
+        System.out.println(a==b);
+
+
+        int C =5;
+        int D=6;
+        System.out.println(a!=b);
+
+
+        int e =5;
+        int f=6;
+        System.out.println(a>b);
+
+
+        int g =5;
+        int h =6;
+        System.out.println(a<=b);
+    }
+}
