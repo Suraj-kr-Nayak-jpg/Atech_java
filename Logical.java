f89a48c78d16a4aeea6c495af4a3d988abb2486c
surajkrnayak <sn1009505@gmail.com>
2025-02-04 07:34:11 -0800

    demo

---
+++ Logical.java
@@ -0,0 +1,22 @@
+public class Logical {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 3;
+        int c = 5;
+        System.out.println(a==b && a==c);
+
+
+        int D= 5;
+        int E = 3;
+        int F = 2;
+        System.out.println(D==E|| D==F);
+
+
+
+        int G= 5;
+        int H = 3;
+        int I = 2;
+        Boolean check =true;
+        System.out.println(!check);
+    }
+}
