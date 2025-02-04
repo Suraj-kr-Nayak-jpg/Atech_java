3fb1f9829f4c52fca8b3bdfb89f9d31023e71509
surajkrnayak <sn1009505@gmail.com>
2025-02-04 08:35:33 -0800

    demo

---
+++ Bitwise_AND.java
@@ -0,0 +1,17 @@
+public class Bitwise_AND {
+    public static void main(String[] args){
+        int a = 5; //0101
+        int b = 6; //0110
+        System.out.println(a&b);
+
+
+        int C = 8; //1000
+        int D = 9; //1001
+        System.out.println(C&D);
+
+
+        int E = 11; //1011
+        int F = 4; //0100
+        System.out.println(E&F);
+    }
+}
