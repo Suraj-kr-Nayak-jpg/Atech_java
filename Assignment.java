84d13723ccb6abfeb4b7e4286f2851decaf2f53d
surajkrnayak <sn1009505@gmail.com>
2025-02-04 05:43:37 -0800

    demo

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+public class Assignment {
+    public static void main(String[] args){
+        int a =19;
+        a+=5;
+        System.out.println(a);
+
+
+        int b =19;
+        b-=5;
+        System.out.println(b);
+
+
+        int C =19;
+        C*=5;
+        System.out.println(C);
+    }
+}
