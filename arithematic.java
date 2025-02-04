8de79b7ff7f8a0c3bb4f8368774a8e19475c638d
surajkrnayak <sn1009505@gmail.com>
2025-02-04 05:36:48 -0800

    demo

--- arithematic.java
+++ arithematic.java
@@ -3,5 +3,20 @@ public class arithematic {
         int n=15;
         int m=3;
         System.out.println(n+m);
+
+
+        int a=15;
+        int b =3;
+        System.out.println(n-m);
+
+
+        int c=15;
+        int d=2;
+        System.out.println(c*d);
+
+
+        int E=16;
+        int F=3;
+        System.out.println(E%F);
     }
 }
