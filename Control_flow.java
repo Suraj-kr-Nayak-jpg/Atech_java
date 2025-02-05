50ae8c18ec7e26ab2827f4f9084b1b2c0a5f0d81
surajkrnayak <sn1009505@gmail.com>
2025-02-05 05:02:54 -0800

    demo

--- Control_flow.java
+++ Control_flow.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
+
 public class Control_flow {
     public static void main(String[] args) {
         int i = 4;
-        if(i<=10) {
-           if(i<=5){
-               System.out.println(" yes i is less then 5");
-           } else{
-               System.out.println("no, i is is not less then 5");
-           }
-        } else if (i<=15) {
+        if (i <= 10) {
+            if (i <= 5) {
+                System.out.println(" yes i is less then 5");
+            } else {
+                System.out.println("no, i is is not less then 5");
+            }
+        } else if (i <= 15) {
             System.out.println("Medium");
-        } else{
+        } else {
             System.out.println("Big");
+
         }
+
     }
 }
