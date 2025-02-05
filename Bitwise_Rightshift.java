26b73cf74c4efa342abb2ec5b8db70873c7bf0ca
surajkrnayak <sn1009505@gmail.com>
2025-02-04 21:54:07 -0800

    demo

---
+++ Bitwise_Rightshift.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+
+public class Bitwise_Rightshift {
+    public static void main(String[] args){
+        int a = 50;
+        int b = a>>1;
+        System.out.println(b);
+
+
+        int c = 50;
+        int d= c>>2;
+        System.out.println(d);
+
+
+        int E = 10;
+        int F = E>>1;
+        System.out.println(F);
+
+
+        int G = 40;
+        int H = G>>2;
+        System.out.println(H);
+    }
+}
