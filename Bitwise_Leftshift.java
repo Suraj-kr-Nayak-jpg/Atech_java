eaadef731d7b40d18fd47c994ddca7ee00669257
surajkrnayak <sn1009505@gmail.com>
2025-02-04 21:19:03 -0800

    demo

---
+++ Bitwise_Leftshift.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class Bitwise_Leftshift {
+    public static void main(String[] args){
+        int a = 5;
+        int b = a<<2;
+        System.out.println(b);
+
+
+
+        int C= 7;
+        int D = C<<3;
+        System.out.println(D);
+
+
+
+        int E = 17;
+        int F = E<<3;
+        System.out.println(F);
+    }
+}
